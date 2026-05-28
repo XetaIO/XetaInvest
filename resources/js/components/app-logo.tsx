a6c4dc6a28add55fdbd84417e01518b44c64f968
Xety <emeric@xetaravel.com>
2026-05-28 19:08:17 +0200

    Add logo on login layout

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,13 +1,9 @@
 export default function AppLogo() {
     return (
         <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md">
-                <img src="/images/logo.png" alt="Logo" />
-            </div>
-            <div className="ml-1 mt-1 grid flex-1.5 text-left">
-                <span className="mb-0.5 truncate leading-tight font-semibold">
-                    Xeta<span className="text-emerald-500">Invest</span>
-                </span>
+            <div className="flex aspect-square items-center justify-center rounded-md">
+                <img src="/images/logo-brand-light-mode.png" alt="Logo" className="h-8 block dark:hidden" />
+                <img src="/images/logo-brand-dark-mode.png" alt="Logo" className="h-8 hidden dark:block" />
             </div>
         </>
     );
--- resources/js/layouts/auth/auth-simple-layout.tsx
+++ resources/js/layouts/auth/auth-simple-layout.tsx
@@ -17,8 +17,9 @@ export default function AuthSimpleLayout({
                             href={home()}
                             className="flex flex-col items-center gap-2 font-medium"
                         >
-                            <div className="mb-1 flex h-9 w-9 items-center justify-center rounded-md">
-                                <AppLogoIcon className="size-9 fill-current text-[var(--foreground)] dark:text-white" />
+                            <div className="mb-1 flex items-center justify-center rounded-md">
+                                <img src="/images/logo-brand-light-mode.png" alt="Logo" className="h-13 block dark:hidden" />
+                                <img src="/images/logo-brand-dark-mode.png" alt="Logo" className="h-13 hidden dark:block" />
                             </div>
                             <span className="sr-only">{title}</span>
                         </Link>
